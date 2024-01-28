aa15687ab87cee2985ce44880abfe3041d85e1aa
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 01:37:14 -0500

    init

---
+++ src/main/java/com/insurgencedev/rivalrodsaddon/RivalRodsAddon.java
@@ -0,0 +1,17 @@
+package com.insurgencedev.rivalrodsaddon;
+
+import com.insurgencedev.rivalrodsaddon.listeners.RivalRodsEventListeners;
+import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
+import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
+
+@IBoostersAddon(name = "RivalFishingRodsAddon", version = "1.0.0", author = "InsurgenceDev", description = "RivalFishingRods Support")
+public class RivalRodsAddon extends InsurgenceBoostersAddon {
+
+    @Override
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("RivalFishingRods")) {
+            registerEvent(new RivalRodsEventListeners());
+        }
+    }
+}
---
+++ src/main/java/com/insurgencedev/rivalrodsaddon/listeners/RivalRodsEventListeners.java
@@ -0,0 +1,33 @@
+package com.insurgencedev.rivalrodsaddon.listeners;
+
+import me.rivaldev.fishingrod.rivalfishingrods.api.RodEssenceReceiveEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
+
+public final class RivalRodsEventListeners implements Listener {
+
+    @EventHandler
+    private void onReceive(RodEssenceReceiveEvent event) {
+        final String TYPE = "Essence";
+        final String NAMESPACE = "RIVAL_RODS";
+        final double[] totalMulti = {1};
+
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
+        }
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
+
+        event.setEssence(calculateAmount(event.getEssence(), totalMulti[0]));
+    }
+
+    private long calculateAmount(double amount, double multi) {
+        return (long) (amount * (multi < 1 ? 1 + multi : multi));
+    }
+}
