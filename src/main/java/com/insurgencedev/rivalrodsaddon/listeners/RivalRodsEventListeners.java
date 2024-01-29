467e8607aaf91bfe8557aafb6eea96760f454d65
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 21:23:21 -0500

    fixed multi

--- src/main/java/com/insurgencedev/rivalrodsaddon/listeners/RivalRodsEventListeners.java
+++ src/main/java/com/insurgencedev/rivalrodsaddon/listeners/RivalRodsEventListeners.java
@@ -12,7 +12,7 @@ public final class RivalRodsEventListeners implements Listener {
     private void onReceive(RodEssenceReceiveEvent event) {
         final String TYPE = "Essence";
         final String NAMESPACE = "RIVAL_RODS";
-        final double[] totalMulti = {1};
+        final double[] totalMulti = {0};
 
         BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
         if (pResult instanceof BoosterFindResult.Success boosterResult) {
@@ -24,7 +24,9 @@ public final class RivalRodsEventListeners implements Listener {
             return null;
         }, () -> null);
 
-        event.setEssence(calculateAmount(event.getEssence(), totalMulti[0]));
+        if (totalMulti[0] > 0) {
+            event.setEssence(calculateAmount(event.getEssence(), totalMulti[0]));
+        }
     }
 
     private long calculateAmount(double amount, double multi) {
