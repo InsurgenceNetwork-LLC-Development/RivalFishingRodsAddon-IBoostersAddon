b40f36d4115ec9e22e75a496e2580f162caf9d9d
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-03-19 04:54:48 -0500

    included perm boosts and reduced code repetition

--- src/main/java/com/insurgencedev/rivalrodsaddon/listeners/RivalRodsEventListeners.java
+++ src/main/java/com/insurgencedev/rivalrodsaddon/listeners/RivalRodsEventListeners.java
@@ -1,57 +1,82 @@
 package com.insurgencedev.rivalrodsaddon.listeners;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import me.rivaldev.fishingrod.rivalfishingrods.api.RodEssenceReceiveEvent;
+import me.rivaldev.fishingrod.rivalfishingrods.api.RodMoneyReceiveEvent;
 import me.rivaldev.fishingrod.rivalfishingrods.api.RodXPGainEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
 import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
+import org.insurgencedev.insurgenceboosters.data.PermanentBoosterData;
+
+import java.util.Optional;
 
 public final class RivalRodsEventListeners implements Listener {
 
+    private final String namespace = "RIVAL_RODS";
+
     @EventHandler
     private void onReceive(RodEssenceReceiveEvent event) {
-        final String TYPE = "Essence";
-        final String NAMESPACE = "RIVAL_RODS";
-        final double[] totalMulti = {0};
+        double multi = getMulti(event.getPlayer(), "Essence");
 
-        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
-        if (pResult instanceof BoosterFindResult.Success boosterResult) {
-            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
+        if (multi > 0) {
+            event.setEssence(calculateAmount(event.getEssence(), multi));
         }
+    }
 
-        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
-            totalMulti[0] += globalBooster.getMultiplier();
-            return null;
-        }, () -> null);
+    @EventHandler
+    private void onReceive(RodMoneyReceiveEvent event) {
+        double multi = getMulti(event.getPlayer(), "Money");
 
-        if (totalMulti[0] > 0) {
-            event.setEssence(calculateAmount(event.getEssence(), totalMulti[0]));
+        if (multi > 0) {
+            event.setMoney(calculateAmount(event.getMoney(), multi));
         }
     }
 
     @EventHandler
     private void onGain(RodXPGainEvent event) {
-        final String TYPE = "Xp";
-        final String NAMESPACE = "RIVAL_RODS";
-        final double[] totalMulti = {0};
+        double multi = getMulti(event.getPlayer(), "XP");
 
-        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
-        if (pResult instanceof BoosterFindResult.Success boosterResult) {
-            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
-        }
-
-        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
-            totalMulti[0] += globalBooster.getMultiplier();
-            return null;
-        }, () -> null);
-
-        if (totalMulti[0] > 0) {
-            event.setXP(calculateAmount(event.getXP(), totalMulti[0]));
+        if (multi > 0) {
+            event.setXP(calculateAmount(event.getXP(), multi));
         }
     }
 
+    private double getMulti(Player player, String type) {
+        AtomicDouble totalMulti = new AtomicDouble(getPersonalPermMulti(player, type) + getGlobalPermMulti(type));
+
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(player).getBoosterDataManager().findActiveBooster(type, namespace);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti.getAndAdd(boosterResult.getBoosterData().getMultiplier());
+        }
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(type, namespace, globalBooster -> {
+            totalMulti.getAndAdd(globalBooster.getMultiplier());
+            return null;
+        }, () -> null);
+
+        return totalMulti.get();
+    }
+
+    private double getPersonalPermMulti(Player uuid, String type) {
+        Optional<PermanentBoosterData> foundMulti = Optional.ofNullable(IBoosterAPI.INSTANCE.getCache(uuid).getPermanentBoosts().getPermanentBooster(type, namespace));
+        return foundMulti.map(PermanentBoosterData::getMulti).orElse(0d);
+    }
+
+    private double getGlobalPermMulti(String type) {
+        AtomicDouble multi = new AtomicDouble(0d);
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findPermanentBooster(type, namespace, data -> {
+            multi.set(data.getMulti());
+            return null;
+        }, () -> null);
+
+        return multi.get();
+    }
+
     private double calculateAmount(double amount, double multi) {
-        return amount * (multi <= 1 ? 1 + multi : multi);
+        return amount * (multi < 1 ? 1 + multi : multi);
     }
 }
